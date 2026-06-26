93ffb9077c1259ad0b8215e1e650df14af6f74ad
Balajisathya09 <balajisathyanarayanan09062004@gmail.com>
2026-06-26 22:43:41 +0530

    Add robots.ts for SEO

---
+++ src/app/robots.ts
@@ -0,0 +1,11 @@
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://balaji09.netlify.app/sitemap.xml',
+  }
+}
